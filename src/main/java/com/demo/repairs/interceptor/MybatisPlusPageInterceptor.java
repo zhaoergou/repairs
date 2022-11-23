af017222f56302b4226aab9da748ce0876f78730
Peach <1269117234@qq.com>
2022-11-23 14:29:14 +0800

    分页

---
+++ src/main/java/com/demo/repairs/interceptor/MybatisPlusPageInterceptor.java
@@ -0,0 +1,19 @@
+package com.demo.repairs.interceptor;
+
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MybatisPlusPageInterceptor {
+    /**
+     * 新版分页插件设置
+     */
+    @Bean
+    public MybatisPlusInterceptor getPaginationInnerInterceptor() {
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor());
+        return interceptor;
+    }
+}
